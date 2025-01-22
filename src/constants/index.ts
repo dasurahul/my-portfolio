d172b96d0a8f80be4ae1642c3ecde3d9be1ea702
Rahul Dasu <rahul@capezero.com>
2025-01-22 11:06:21 +0530

    update cv

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
 export const LINKED_PROFILE_URL = "https://linkedin.com/in/rahuldasu";
 export const GITHUB_PROFILE_URL = "https://github.com/dasurahul";
 export const CV_URL =
-  "https://drive.google.com/file/d/15cTV4MGtkwvY2-JsZPeeAUXejggP3Xb3/view?usp=sharing";
+  "https://drive.google.com/file/d/1SFp3G8W3vgY6K13rkLiGmw44fXV6VEOD/view?usp=sharing";
